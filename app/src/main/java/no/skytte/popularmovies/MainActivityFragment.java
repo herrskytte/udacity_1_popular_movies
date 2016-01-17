7a25b7a6995e36b8ae83dfe5360eceedbc04a738
Frederik Skytte <fsk@computas.com>
2016-01-17 20:07:10 +0100

    Initial commit

---
+++ app/src/main/java/no/skytte/popularmovies/MainActivityFragment.java
@@ -0,0 +1,37 @@
+package no.skytte.popularmovies;
+
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_main, container, false);
+        ButterKnife.bind(this, v);
+        return v;
+    }
+
+    @OnClick({R.id.btn_spotify, R.id.btn_scores,R.id.btn_lib, R.id.btn_build, R.id.btn_xyz, R.id.btn_cap})
+    public void clickStartApp(Button button){
+        Snackbar.make(button, "When complete, this will start: " + button.getText(), Snackbar.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
+}
